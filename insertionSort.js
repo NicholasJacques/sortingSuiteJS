5df082f68af9d7038b8f1116f14d41496baf6ed7
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-07-02 22:35:12 -0600

    completes insertionsort

---
+++ insertionSort.js
@@ -0,0 +1,16 @@
+// run "mocha test/insertionSort-test.js" in terminal to test
+
+function insertionSort(array) {
+  for (i = 1; i < array.length; i++) {
+    j = i
+    while (j > 0 && array[j - 1] > array[j]) {
+      temp = array[j-1]
+      array[j-1] = array[j]
+      array[j] = temp
+      j -= 1
+    }
+  }
+  return array
+}
+
+module.exports = insertionSort
